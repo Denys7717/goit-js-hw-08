8bb514c62471f43236f070ac871f5aee16d78f67
Denys7717 <denys.rdbua@gmail.com>
2023-07-17 16:00:56 +0300

    updare1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryElem = document.querySelector('.gallery');
+
+const imgListArr = galleryItems.map(
+  image =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+);
+const template = imgListArr.join('');
+galleryElem.insertAdjacentHTML('beforeend', template);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
